e1d1b979df2a2f181b44025fecf71b19e2ac5f72
anup73 <wadekaranup@gmail.com>
2026-02-22 21:03:27 +0530

    commit

--- src/main/java/com/example/demo/DemoApplicationStartApp.java
+++ src/main/java/com/example/demo/DemoApplicationStartApp.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan("com")
 public class DemoApplicationStartApp {
 
