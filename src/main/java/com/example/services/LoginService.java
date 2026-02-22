bc37d4dfd2897c63554833925343f7f783cf74fe
anup73 <wadekaranup@gmail.com>
2026-02-22 19:00:43 +0530

    commit

--- src/main/java/com/example/services/LoginService.java
+++ src/main/java/com/example/services/LoginService.java
@@ -1,6 +1,7 @@
 package com.example.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
